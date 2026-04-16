bae0d1dbaf4d226cd9727fbcfd0ee2976b472d3f
Anas Khan <anaskhanofficial005@gmail.com>
2026-04-16 22:15:37 +0530

    solve mongodb issue

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ const dbConnect = async () => {
   try {
     const connection = await mongoose.connect(
     //   'mongodb+srv://riteshpatidar088:j0eG38RfUP1AROum@cluster0.b2vgi2d.mongodb.net/restaurent-qr?appName=Cluster0'
-     'mongodb+srv://Restaurant-qr-db:v4fhqhnd@cluster0.2gdisay.mongodb.net/restaurent-qr-system?appName=Cluster0'
+     'mongodb://Restaurant-qr-db:v4fhqhnd@ac-wbinv4r-shard-00-00.2gdisay.mongodb.net:27017,ac-wbinv4r-shard-00-01.2gdisay.mongodb.net:27017,ac-wbinv4r-shard-00-02.2gdisay.mongodb.net:27017/restaurent-qr-system?ssl=true&replicaSet=atlas-urd40b-shard-0&authSource=admin&appName=Cluster0'
     );
     console.log('db connected');
   } catch (error) {
